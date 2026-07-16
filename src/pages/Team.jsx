72fa0463126534b4a9cfe5d272dffd7f52b4385c
Mach808 <mananchoudhary1995@gmail.com>
2026-07-16 14:53:41 +0530

    add '2026' year in teams.js page'

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -23,7 +23,7 @@ const socialMediaIcons = {
 
 const Team = () => {
   const [teamData, setTeamData] = useState([]);
-  const [year, setYear] = useState("2025");
+  const [year, setYear] = useState("2065");
   const [selectedTab, setSelectedTab] = useState("Advisory");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
